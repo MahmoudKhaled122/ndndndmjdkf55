1c490dd7f0190bf7e20bf77961043a1057b5ff2c
MahmoudKhaled122 <m.khalid100200@gmail.com>
2025-08-09 10:31:37 +0300

    Add files via upload

---
+++ ResponsiveSimpleCalculatorAssets/SimpleCalculatorManipulation/js/index.js
@@ -0,0 +1,34 @@
+// var fristHeading = document.querySelector(".responsive-simple-calculator-container .responsive-simple-calculator-heading-container #mainHeading");
+// // console.log(fristHeading);
+// var fristDescription = document.querySelector(".responsive-simple-calculator-container .responsive-simple-calculator-heading-container #mainDescription");
+// // console.log(fristDescription);
+// var userName = window.prompt("Please Enter The User Name Is : ")
+// // console.log(userName);
+// if(!userName){
+//     userName = "guest";
+// }
+// // console.log(userName);
+// function displayingMainDescription(){
+//     fristHeading.addEventListener("click" , function(){
+//         fristDescription.style.display="block";
+//         fristDescription.innerHTML = `hello everyone! , and welcome \"${userName}\" to the responsive simple calculator , you can solve different mathimaticas equations by using simple calculator`;
+//     });
+// };
+// displayingMainDescription();
+// var displayingUserValue = document.querySelector(".responsive-simple-calculator-container .responsive-simple-calculator-buttons-container #displayingValue");
+// // console.log(displayingUserValue);
+// function appendUserValue(displayValue){
+//     displayingUserValue.value += displayValue;
+// };
+// function clearingUserValue(){
+//     displayingUserValue.value="";
+// };
+// function calculatingMathimaticasEquations(){
+//     try{
+//         displayingUserValue.value = eval(displayingUserValue.value);
+//     }
+//     catch{
+//         displayingUserValue.value="mathimaticas syntax error";
+//     };
+// };
+
